b661b521023bd4c2c76b5f17d088a1e1ca968501
Andrey Moscovets <106304526+andmosc@users.noreply.github.com>
2022-12-17 21:31:29 +0900

    Revert "new branch feature/handlers"
    This reverts commit 1a210397957e0674863ef297c8646bc5435d1dc6.

--- src/main/java/ru/andmosc/Main.java
+++ src/main/java/ru/andmosc/Main.java
@@ -5,25 +5,7 @@ public class Main {
     public static final int PORT = 8080;
     public static final int SIZE_POOL = 64;
     public static void main(String[] args) {
-        public static void main(String[] args){
-            final var server = new Server();
-            // код инициализации сервера (из вашего предыдущего ДЗ)
-
-            // добавление handler'ов (обработчиков)
-            server.addHandler("GET", "/messages", new Handler() {
-                public void handle(Request request, BufferedOutputStream responseStream) {
-                    // TODO: handlers code
-                }
-            });
-            server.addHandler("POST", "/messages", new Handler() {
-                public void handle(Request request, BufferedOutputStream responseStream) {
-                    // TODO: handlers code
-                }
-            });
-
-            server.listen(9999);
-        }
-
+        new Server(PORT,SIZE_POOL);
     }
 }
 
